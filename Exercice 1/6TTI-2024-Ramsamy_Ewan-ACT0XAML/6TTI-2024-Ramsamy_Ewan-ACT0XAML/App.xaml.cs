03991dbe101baa63d2a732738efda86538ffb710
EwanR5 <91669749+EwanR5@users.noreply.github.com>
2023-09-08 13:47:21 +0200

    Add files via upload

---
+++ Exercice 1/6TTI-2024-Ramsamy_Ewan-ACT0XAML/6TTI-2024-Ramsamy_Ewan-ACT0XAML/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _6TTI_2024_Ramsamy_Ewan_ACT0XAML
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
